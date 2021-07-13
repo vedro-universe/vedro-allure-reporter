76403d603b7a882495b35d7a0b8007837466e2dc
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2021-07-13 23:48:11 +0700

    bump version → 0.2.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-allure-reporter",
-    version="0.1.0",
+    version="0.2.0",
     description="",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_allure_reporter/__init__.py
+++ vedro_allure_reporter/__init__.py
@@ -1,4 +1,4 @@
 from ._allure_reporter import AllureReporter
 
-__version__ = "0.1.0"
+__version__ = "0.2.0"
 __all__ = ("AllureReporter",)
