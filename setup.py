d761be4a282cc01c7ea828d258bdd22666fd33fa
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-27 19:28:38 +0300

    update python version

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Nikita Tsvetkov",
     author_email="nikitanovosibirsk@yandex.com",
-    python_requires=">=3.8",
+    python_requires=">=3.7",
     url="https://github.com/nikitanovosibirsk/vedro-allure-reporter",
     license="Apache-2.0",
     packages=find_packages(exclude=("tests",)),
@@ -28,6 +28,7 @@ setup(
     tests_require=find_dev_required(),
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
