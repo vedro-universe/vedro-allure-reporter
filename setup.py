5a8b9b34e8e36a23e83acbfc5b685ff451126f25
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-27 19:49:59 +0300

    bump version → 1.0.0

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-allure-reporter",
-    version="0.2.5",
+    version="1.0.0",
     description="Allure reporter for Vedro framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
--- vedro_allure_reporter/__init__.py
+++ vedro_allure_reporter/__init__.py
@@ -2,5 +2,5 @@ from allure_commons.model2 import Label as AllureLabel
 
 from ._allure_reporter import AllureReporter, AllureReporterPlugin
 
-__version__ = "0.2.5"
+__version__ = "1.0.0"
 __all__ = ("AllureReporter", "AllureReporterPlugin", "AllureLabel",)
