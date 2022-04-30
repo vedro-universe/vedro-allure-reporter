f785a691cae6e7b308fa5a5e5110db374f9d3750
Nikita Tsvetkov <nikitanovosibirsk@yandex.com>
2022-04-30 22:08:07 +0300

    bump version → 1.1.0

--- vedro_allure_reporter/__init__.py
+++ vedro_allure_reporter/__init__.py
@@ -2,5 +2,5 @@ from allure_commons.model2 import Label as AllureLabel
 
 from ._allure_reporter import AllureReporter, AllureReporterPlugin
 
-__version__ = "1.0.0"
+__version__ = "1.1.0"
 __all__ = ("AllureReporter", "AllureReporterPlugin", "AllureLabel",)
