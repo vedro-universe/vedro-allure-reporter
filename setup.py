6b580645fb1651a03de00f25c5c9aea548b28a51
Nikita Tsvetkov <tsv1@fastmail.com>
2024-01-16 17:03:07 +0400

    bump version → 1.8.1

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ def find_dev_required():
 
 setup(
     name="vedro-allure-reporter",
-    version="1.8.0",
+    version="1.8.1",
     description="Allure reporter for Vedro testing framework",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
