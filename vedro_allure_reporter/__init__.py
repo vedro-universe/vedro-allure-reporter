68b4ef4b83d215dde206c8ea3afbbafd7f74f0ba
Nikita Tsvetkov <tsv1@fastmail.com>
2025-11-07 19:29:30 +0400

    Add allure_step to __all__ export list

--- vedro_allure_reporter/__init__.py
+++ vedro_allure_reporter/__init__.py
@@ -1,9 +1,9 @@
+from allure_commons._allure import step as allure_step
 from allure_commons.model2 import Label as AllureLabel
 
 from ._allure_labels import Epic, Feature, Story, allure_labels
 from ._allure_reporter import AllureReporter, AllureReporterPlugin
-from ._allure_steps import AllureStepHooks
 
 __version__ = "1.11.1"
 __all__ = ("AllureReporter", "AllureReporterPlugin", "AllureLabel",
-           "Epic", "Story", "Feature", "allure_labels", "AllureStepHooks")
+           "Epic", "Story", "Feature", "allure_labels", "allure_step")
